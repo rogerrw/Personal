0202dc201a24247664627c0edb8fdbbd5b04c4ba
Roger <roger.wang.r@gmail.com>
2017-08-11 15:26:11 -0700

    add footer element, flickr logo

---
+++ site/containers/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+require('../styles/Footer.scss');
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href='#'>
+          <img src="/images/flickr_logo.png" alt="" />
+        </a>
+      </div>
+    );
+  }
+}
--- site/index.js
+++ site/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import Footer from './containers/Footer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
