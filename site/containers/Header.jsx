370b13bb003224bf03afc53b8d4a3bcbde55cc27
Roger Wang <roger.wang.r@gmail.com>
2018-05-29 15:09:26 -0400

    new fonts, header and footer

--- site/containers/Header.jsx
+++ site/containers/Header.jsx
@@ -5,6 +5,14 @@ export default class Header extends React.Component {
   render() {
     return (
       <div>
+        <div id="nav">
+          <a href="#skills">Skills</a>
+          <a href="#portfolio">Portfolio</a>
+        </div>
+        <h1>Roger Wang</h1>
+        <div id="resume-download">
+          <a target="_blank" href="/resume.pdf">Resume</a>
+        </div>
       </div>
     );
   }
