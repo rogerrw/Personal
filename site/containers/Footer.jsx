77598dfed58f6263bcf9558335a4e5986675823b
Roger <roger.wang.r@gmail.com>
2018-06-04 16:19:32 -0400

    add footer logos/external links

--- site/containers/Footer.jsx
+++ site/containers/Footer.jsx
@@ -5,8 +5,14 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div>
-        <a href='https://www.linkedin.com/in/roger-wang/'>
-          LinkedIn
+        <a target='_blank' href='https://www.linkedin.com/in/roger-wang/'>
+          <img src='../../images/linkedin_logo.png' />
+        </a>
+        <a target='_blank' href='https://stackoverflow.com/users/2502953/rogerwang'>
+          <img src='../../images/stackoverflow_logo.png' />
+        </a>
+        <a target='_blank' href='https://github.com/rogerrw'>
+          <img src='../../images/github_logo.png' />
         </a>
       </div>
     );
